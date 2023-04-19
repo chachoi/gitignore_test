f51249a5e226d9ad37fd29f3af47e1f366d086ca
James Choi <chachoi@blackberry.com>
2023-04-19 10:04:13 -0400

    t

--- build_qnx/nto/aarch64/le/build/googlemock/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/chachoi/upstream/googletest/googlemock
-# Build directory: /home/chachoi/upstream/googletest/build_qnx/nto/aarch64/le/build/googlemock
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("../googletest")
--- build_qnx/nto/x86_64/so/build/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/chachoi/upstream/googletest
-# Build directory: /home/chachoi/upstream/googletest/build_qnx/nto/x86_64/so/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("googlemock")
